3110dd238ca2e002bd44c0197c4c695fc1b919d9
Israel <israel.coelho@adviseu.com.br>
2022-01-26 15:53:59 -0300

    fix: ts-config-paths added and its configuration

--- src/controllers/AnimalsController.ts
+++ src/controllers/AnimalsController.ts
@@ -1,9 +1,11 @@
+import Animal from '@models/Animal';
 import { Request, Response } from 'express';
-import Animal from '../models/Animal';
 
 class AnimalController {
     public async create(req: Request, res: Response): Promise<Response> {
+        const animal = new Animal();
         const reqJson: JSON = req.body;
+
         return res.json(reqJson); 
     }
 }
