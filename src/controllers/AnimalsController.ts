82b85d3d1edf444f3454a27ce2615118f4ee89f9
ipCoelho <ipcoelho.business@gmail.com>
2022-02-01 15:45:56 -0300

    feat: AnimalsController finished

--- src/controllers/AnimalsController.ts
+++ src/controllers/AnimalsController.ts
@@ -3,7 +3,31 @@ import Animal from '@models/Animal';
 
 class AnimalController {
     public async create(req: Request, res: Response): Promise<Response> {
-        const animal = new Animal(req.body);
+        const animal = await Animal.create(req.body);
+
+        return res.json(animal); 
+    }
+
+    public async read(req: Request, res: Response): Promise<Response> {
+        const animal = await Animal.find();
+
+        return res.json(animal); 
+    }
+
+    public async readID(req: Request, res: Response): Promise<Response> {
+        const animal = await Animal.findById(req.body);
+
+        return res.json(animal); 
+    }
+
+    public async update(req: Request, res: Response): Promise<Response> {
+        const animal = await Animal.findByIdAndUpdate(req.body);
+
+        return res.json(animal);
+    }
+
+    public async delete(req: Request, res: Response): Promise<Response> {
+        const animal = await Animal.deleteOne(req.body);
 
         return res.json(animal); 
     }
