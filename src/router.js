14c7a898f5305819afec7371dc091d8e841763ff
ipCoelho <ipcoelho.business@gmail.com>
2022-03-18 16:48:42 -0300

    feat: project on progress

--- src/router.js
+++ src/router.js
@@ -1,10 +1,21 @@
 import { Router } from "express";
+import Prisma from '@prisma/client'
+const {PrismaClient} = Prisma;
 
-const router = Router();
+const prisma = new PrismaClient();
+
+const router = Router();  
 
 // Ong Management
 router.get("/ong/search", (req, res) => {
+  const allOngs = prisma.tbl_ong.findMany({});
+  // console.log(`\nreq.body: ${body}\nreq: ${req}`);
   return res.json({message: "hello"});
+});
+
+router.get("/ong/search?nomeOng", (req, res) => {
+  const nomeOng = req.
+  res.json({message: "ola"})
 });
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -5,8 +5,10 @@ import router from './router.js';
 const API = express();
 const port = 3030;
 
-API.listen(port);
+API.listen(3030, ()=>{console.log('TESTE')});
 console.log(`\n > The API is listening the port ${port}`);
 
-API.use(cors()).use(router);
+API.use('/', router);
+
+
   
