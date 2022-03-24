524721e1b19c2a65def4e9170de80607876d2827
Israel Coelho <israel.coelho@adviseu.com.br>
2022-03-24 10:07:24 -0300

    feat: migration to Ts done

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './router';
+import router from './router';
+class API {
+  api;
+  framework;
+  host;
+  port;
+  router;
 
-const API = express();
-const port = process.env["API_PORT"] ?? 3030;
+  constructor(framework, host, port, router) {
+    this.api = framework();
+    this.framework = framework;
+    this.host = host;
+    this.port = port;
+    this.router = router;
+  }
 
-API.listen(port);
-console.log(`\n > The API is listening the port ${port}`);
+  start() {
+    this.api.listen(this.port, () => console.log(`\n > API listening at: ${this.host}${this.port}/`));
+    this.api
+      .use(this.framework.json())
+      .use(cors())
+      .use('/', this.router);
+  }
+}
 
-API.use(cors()).use(routes);
-console.log();
-
+const api = new API(express, "http://localhost:", process.env.API_PORT ?? 3030, router);
+api.start(); 
