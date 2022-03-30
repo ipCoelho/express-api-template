f39e0a872d22d7fa998fe7acab6a6c4a0ad22469
ipCoelho <ipcoelho.business@gmail.com>
2022-03-30 16:42:58 -0300

    feat: login method added to the class

--- src/controllers/OngController.ts
+++ src/controllers/OngController.ts
@@ -1,43 +1,74 @@
-import { Request, Response } from 'express';
-import {PrismaClient} from "@prisma/client";
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 class OngController {
- async preRegister(req: Request, res: Response) {
-   try {
-     const {cnpj, nome, email, senha} = req.body;
-     const preCadastroLogin = await prisma.tbl_login.create({ 
-       data: {
-         email: email,
-         senha: senha 
-       }
-     });
+  async preRegister(req: Request, res: Response) {
+    try {
+      const { cnpj, nome, email, senha } = req.body;
+      const preCadastroLogin = await prisma.tbl_login.create({
+        data: {
+          email: email,
+          senha: senha,
+        },
+      });
 
-     const {idLogin} = await prisma.tbl_login.findFirst({
-       where: {email: email, senha: senha}
-     });
-  
-     const preCadastroOng = await prisma.tbl_ong.create({
-       data: {
-         idLogin: idLogin,
-         cnpj: cnpj,
-         nome: nome,
-         dataDeCriacao: new Date().toISOString(),
-       }
-     });
-     res.status(200);
-     res.json({ ResquestData: req.body, DatabaseResponse: preCadastroOng });
-   } catch (error) {
-     res.status(500);
-     res.json({ RequestData: req.body, DatabaseResponse: error });
-   }
- }
+      const { idLogin } = await prisma.tbl_login.findFirst({
+        where: { email: email, senha: senha },
+      });
+
+      const preCadastroOng = await prisma.tbl_ong.create({
+        data: {
+          idLogin: idLogin,
+          cnpj: cnpj,
+          nome: nome,
+          dataDeCriacao: new Date().toISOString(),
+        },
+      });
+      res.status(200);
+      res.json({ ResquestData: req.body, DatabaseResponse: preCadastroOng });
+    } catch (error) {
+      res.status(500);
+      res.json({ RequestData: req.body, DatabaseResponse: error });
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    try {
+      const { email, senha } = req.body;
+      const { idLogin, ...loginData } = await prisma.tbl_login.findUnique({
+        where: { email: email },
+      });
+
+      if (loginData.senha === senha) {
+        const ongData = await prisma.tbl_ong.findFirst({
+          where: { idLogin: idLogin },
+        });
+        res.status(200);
+        res.json({
+          message: "E-mail and password matched.",
+          RequestData: req.body,
+          DatabaseResponse: ongData,
+        });
+      } else {
+        res.status(400);
+        res.json({
+          message: "E-mail and password not matched.",
+          RequestData: req.body,
+          DatabaseResponse: {},
+        });
+      }
+    } catch (error) {
+      res.status(500);
+      res.json({ RequestData: req.body, DatabaseResponse: error });
+    }
+  }
 
   async read(req: Request, res: Response) {
     const requestData = req.body;
     const databaseData = await prisma.tbl_ong.findMany({});
-    
+
     res.status(200);
     res.json({
       RequestData: requestData,
@@ -47,30 +78,32 @@ class OngController {
 
   async readID(req: Request, res: Response) {
     const data = req.params.id || req.params.idOng;
-    const databaseData = await prisma.tbl_ong.findUnique({where: {idOng: parseInt(data)} });
+    const databaseData = await prisma.tbl_ong.findUnique({
+      where: { idOng: parseInt(data) },
+    });
 
     res.status(200);
     res.json({ id: data, DatabaseResponse: databaseData });
   }
-  
+
   async create(req: Request, res: Response) {
     const data = req.body;
 
     const databaseData = await prisma.tbl_ong.create({
-      data: {...data}
+      data: { ...data },
     });
 
     res.status(200);
     res.json({ RequestData: data, DatabaseResponse: databaseData });
- }
+  }
 
-   async update(req: Request, res: Response) {
+  async update(req: Request, res: Response) {
     const id = req.params.id || req.params.idOng;
     const data = req.body;
 
     const databaseData = await prisma.tbl_ong.update({
-      where: {idOng: parseInt(id)},
-      data: {...data}
+      where: { idOng: parseInt(id) },
+      data: { ...data },
     });
 
     res.status(200);
@@ -81,7 +114,7 @@ class OngController {
     const data = req.params.id || req.params.idOng;
 
     const databaseData = await prisma.tbl_ong.delete({
-      where: {idOng: parseInt(data)},
+      where: { idOng: parseInt(data) },
     });
 
     res.status(200);
