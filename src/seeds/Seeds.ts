6387d5adc2f7082c7704cd103f498fccd57b12a9
Israel Coelho <israel.coelho@adviseu.com.br>
2022-04-04 10:02:33 -0300

    feat: data to feed the seeds exported to a file, and seeds transformed to a Class

---
+++ src/seeds/Seeds.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+
+export class Seeds {
+  async tableEstado(dataEstado) {
+    try {
+      await prisma.tbl_estado.createMany({data: dataEstado});
+      console.log("> tbl_estado seeded.");
+    } catch (error) {
+      console.log(`> tbl_estado `, `WAS NOT SEEDED\n`, error);
+    }
+  }
+
+  async tableLogin(loginData) {
+    try {
+      await prisma.tbl_login.createMany({data: loginData});
+      console.log("> tbl_login seeded.")
+    } catch (error) {
+      console.log(`> tbl_login `, `WAS NOT SEEDED\n`, error);
+    }
+  }
+
+  async tableOng(ongData) {
+    ongData.map((ong) => {
+      ong.dataDeCriacao = new Date().toISOString();
+      ong.dataDeFundacao = new Date().toISOString();
+    });
+  
+    try {
+      await prisma.tbl_ong.createMany({data: ongData});
+      console.log("> tbl_ong seeded.")
+    } catch (error) {
+      console.log(`> tbl_ong `, `WAS NOT SEEDED\n`, error);
+    }
+  }
+}
