cbcbc060d45b19ba110f19ce547e5046cfd6b6b7
ipCoelho <ipcoelho.business@gmail.com>
2022-04-25 14:16:10 -0300

    feat: UFcontroller and router added

--- src/controllers/FavoriteController.ts
+++ src/controllers/FavoriteController.ts
@@ -5,57 +5,16 @@ const prisma = new PrismaClient();
 
 class FavoriteController {
     async create(req: Request, res: Response) {
-        // try {
-        //     const fav = req.body;
+    //     const { userId, postId } = req.body;
+    //     const favorite = await prisma.favorite.create({
+    //         data: {
+    //             user: { connect: { id: userId } },
+    //             post: { connect: { id: postId } }
+    //         }
+    //     });
+    //     return res.json(favorite);
+    // }
 
-        //     if (fav.idOng === undefined || fav.idUsuario === undefined) {
-        //         return res.status(400).json({
-        //             message: "Missing data.",
-        //             expected: {
-        //                 idOng: "Number",
-        //                 idUsuario: "Number",
-        //             },
-        //         });
-        //     }
-
-        //     const verification = await prisma.tbl_favoritos.findMany({
-        //         where: {
-        //             idOng: fav.idOng,
-        //             idUsuario: fav.idUsuario,
-        //         }
-        //     });
-
-        //     if (verification.length === 0) {
-        //         await prisma.tbl_favoritos.create({
-        //             data: {
-        //                 idOng: fav.idOng,
-        //                 idUsuario: fav.idUsuario,
-        //                 favorito: 1,
-        //             }
-        //         });
-
-        //         return res.status(200).json({
-        //             message: "Favorite didn't exist, but created.",
-        //             status: true,
-        //         });
-        //     } else if (verification.length > 0) {
-        //         const status = verification[0].favorito === 0 ? true : false;
-
-        //         // await prisma.tbl_favoritos.update({
-        //         // });     
-
-        //         return res.status(200).json({
-        //             message: `Favorito already exists as '${!status}', setting an opposite status.`,
-        //             status: status,
-        //         });
-        //     }
-        // } catch (error) {
-        //     console.log(error);
-        //     return res.status(500).json({
-        //         message: "Internal server error.",
-        //         status: false,
-        //     });
-        // }
     }
 }
 
---
+++ src/routes/ufRouter.ts
@@ -0,0 +1,10 @@
+import UFController from "@controllers/UFController";
+import { Router } from "express";
+
+const router = Router();
+const ufController = new UFController();
+
+router.get("/uf/", (req, res) => {
+    console.info(`> Request GET recieved in '/uf/all' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+    ufController.findAll(req, res);
+});
