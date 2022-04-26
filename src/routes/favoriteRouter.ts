4d1eca02850cf710950c8296973ff55f04173e14
Israel <ipcoelho.business@gmail.com>
2022-04-26 13:55:37 -0300

    feat: gestão de favoritos terminada

--- src/routes/favoriteRouter.ts
+++ src/routes/favoriteRouter.ts
@@ -9,26 +9,26 @@ router.post("/favorite", (req, res) => {
     favController.create(req, res);
 });
 
-router.delete("/favorite/:id", (req, res) => {
-    console.info(`> Request DELETE recieved in '/favorite/:id' at ${new Date().toLocaleString()}. \n> req.params:`, req.params);
+router.delete("/favorite/:idUsuario/:idOng", (req, res) => {
+    console.info(`> Request DELETE recieved in '/favorite' at ${new Date().toLocaleString()}. \n> req.params:`, req.params);
     favController.delete(req, res);
 });
 
-router.get("/favorite", (req, res) => {
-    console.info(`> Request GET recieved in '/favorite' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
-    favController.find(req, res);
-});
+// router.get("/favorite", (req, res) => {
+//     console.info(`> Request GET recieved in '/favorite' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+//     favController.find(req, res);
+// });
 
 
 router.get("/favorite/:idUsuario", (req, res) => {
     console.info(`> Request GET recieved in '/favorite/:usuario' at ${new Date().toLocaleString()}. \n> req.params:`, req.params);
-    favController.findUser(req, res);
+    favController.allOngsPerUser(req, res);
 });
 
-router.get("/favorite/:usuario/:ong", (req, res) => {
-    console.info(`> Request GET recieved in '/favorite/:usuario/:ong' at ${new Date().toLocaleString()}. \n> req.params:`, req.params);
-    favController.findUserAndOng(req, res);
-});
+// router.get("/favorite/:usuario/:ong", (req, res) => {
+//     console.info(`> Request GET recieved in '/favorite/:usuario/:ong' at ${new Date().toLocaleString()}. \n> req.params:`, req.params);
+//     favController.findUserAndOng(req, res);
+// });
 
 
 export default router;
