9fc08fda0afa9ec17272356b868891c398c18e28
ipCoelho <ipcoelho.business@gmail.com>
2022-05-02 15:37:11 -0300

    error: ufcontroller.filterByUf NOT finished

--- src/controllers/UFController.ts
+++ src/controllers/UFController.ts
@@ -29,6 +29,63 @@ class UFController {
 			});
 		}
 	}
+
+	async filterByUf(req: Request, res: Response) {
+		try {
+			if (!req.body.uf) {
+				return res.status(400).json({
+					message: "UF não informada.",
+					status: 400,
+					expected: {
+						uf: "string",
+					},
+				});
+			}
+			console.log("req.body.uf: ", req.body.uf);
+			
+			const getUfId = await prisma.tbl_estado.findUnique({
+				where: {
+					nome: req.body.uf,
+				},
+			});
+			console.log("getUfId: ", getUfId);
+			
+
+			if (getUfId == null) {
+				return res.status(404).json({
+					message: "UF não encontrada.",
+					status: 404,
+				});
+			}
+
+			const ongsId = await prisma.tbl_ong_estado.findMany({
+				where: {
+					idEstado: getUfId.idEstado,
+				},
+			});
+			console.log("ongsId: ", ongsId);
+
+			if (ongsId.length > 0) {
+				return res.status(200).json({
+					message: `Lista de ONGs para o estado '${req.body.uf}' com sucesso.`,
+					status: 200,
+					data: ongsId,
+				});
+			} else {
+				return res.status(404).json({
+					message: `Nenhuma ONG encontrada para o estado '${req.body.uf}'.`,
+					status: 404,
+				});
+			}
+
+		} catch (error) {
+			console.log("Error: ", error);
+			return res.status(500).json({
+				message: process.env.ERRO_500 ?? "Erro no servidor.",
+				status: 500,
+			});
+		}
+	}
 }
 
 export default UFController;
--- src/routes/ufRouter.ts
+++ src/routes/ufRouter.ts
@@ -9,4 +9,9 @@ router.get("/uf", (req, res) => {
     ufController.findAll(req, res);
 });
 
+router.post("/uf/filter", (req, res) => {
+    console.info(`> Request POST recieved in '/uf/filter' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+    ufController.filterByUf(req, res);
+});
+
 export default router;
