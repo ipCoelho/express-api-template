a76a0c56cd591056b241c748a2accac6672f5f38
ipCoelho <ipcoelho.business@gmail.com>
2022-05-02 13:57:34 -0300

    fix: endpoints altered to bank-data

--- src/routes/bankdataRouter.ts
+++ src/routes/bankdataRouter.ts
@@ -4,27 +4,27 @@ import { Router } from "express";
 const router = Router();
 const bankDateController = new BankDateController();
 
-router.post("/bankdata", (req, res) => {
+router.post("/bank-data", (req, res) => {
   console.info(`> Request POST recieved in '/bankdata' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
   bankDateController.create(req, res);
 });
 
-router.get("/bankdata", (req, res) => {
+router.get("/bank-data", (req, res) => {
   console.info(`> Request GET recieved in '/bankdata' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
   bankDateController.findAll(req, res);
 });
 
-router.get("/bankdata/:id", (req, res) => {
+router.get("/bank-data/:id", (req, res) => {
   console.info(`> Request GET recieved in '/bankdata/:id' at ${new Date().toLocaleString()}. \n> req.params:`, req.params);
   bankDateController.findById(req, res);
 });
 
-router.put("/bankdata/:id", (req, res) => {
+router.put("/bank-data/:id", (req, res) => {
   console.info(`> Request PUT recieved in '/bankdata/:id' at ${new Date().toLocaleString()}. \n> req.body:`, req.body, `\n> req.params:`, req.params);
   bankDateController.update(req, res);
 });
 
-router.delete("/bankdata/:id", (req, res) => {
+router.delete("/bank-data/:id", (req, res) => {
   console.info(`> Request DELETE recieved in '/bankdata/:id' at ${new Date().toLocaleString()}. \n> req.params:`, req.params);
   bankDateController.remove(req, res);
 });
