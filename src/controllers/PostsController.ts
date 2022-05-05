2c3ac22c50787de7f11046bc4155216a4ca5ca66
ipCoelho <ipcoelho.business@gmail.com>
2022-05-05 11:22:59 -0300

    feat: features findAll finished and update on progress

--- src/controllers/PostsController.ts
+++ src/controllers/PostsController.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-class PostsController {
+class allPostsController {
   async create(req: Request, res: Response) {
     try {
       if (!req.body.idOng || !req.body.descricao) {
@@ -61,7 +61,134 @@ class PostsController {
     }
   }
 
+  async findAll(req: Request, res: Response) {
+    try {
+      const allPosts = await prisma.tbl_post.findMany(
+       {
+         include: {
+          tbl_ong: true,
+          tbl_post_media: true,
+         },
+       }
+      );
+    
+      return res.status(200).json({
+        message: "Posts encontrados com sucesso.",
+        status: 200,
+        data: allPosts,
+      });
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return res.status(500).json({
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+        status: 500,
+      });
+    }
+  }
 
+  async findById(req: Request, res: Response) {
+    try {
+      const post = await prisma.tbl_post.findMany({
+        where: {
+          idOng: Number(req.params.id),
+        },
+        include: {
+          tbl_ong: true,
+          tbl_post_media: true,
+        },
+      });
+
+      if (post) {
+        return res.status(200).json({
+          message: `Posts da ONG '${req.params.id}' encontrado com sucesso.`,
+          status: 200,
+          data: post,
+        });
+      }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return res.status(500).json({
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+        status: 500,
+      });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      if (
+        !req.body.descricao &&
+        !req.body.media
+      ) {
+        return res.status(400).json({
+          message: "Dados incompletos.",
+          expected: {
+            descricao: "string",
+            media: {
+              idPostMedia: "number",
+              titulo: "string",
+              endereco: "string?",
+            },  
+          },
+          status: 400,
+        });
+      }
+
+      console.log(req.params);
+      const request = req.body;
+      request.idOng = Number(req.params.idOng);
+      request.idPost = Number(req.params.idPost);
+      request.descricao = req.body.descricao;
+      request.media = req.body.media;
+
+      console.log(`Request Inteface: `, request);
+
+      const postVerify = await prisma.tbl_post.findUnique({
+        where: {
+          idPost: Number(req.params.idPost)
+        },
+      });
+      
+      console.log(`Post verif: `, postVerify);
+
+      if (postVerify != null) {
+        const postUpdate = await prisma.tbl_post.update({
+          where: {
+            idPost: Number(request.idPost),
+          },
+          data: {
+            descricao: request.descricao,
+            
+          },
+        });
+          console.log(`PostUpdated: `, postUpdate);
+      }
+
+
+      // for (let i = 0; i < request.media.length; i++) {
+      //   const mediaUpdate = await prisma.tbl_post_media.update({
+      //     where: {
+      //       idPostMedia: Number(request.media[i].idPostMedia),
+      //       idOng: Number(request.idOng),
+      //     },
+      //     data: {
+      //       titulo: request.media[i].titulo,
+      //       endereco: request.media[i].endereco,
+      //     },
+      //   });
+      //   console.log(`Media[${i}]:\n ${JSON.stringify(mediaUpdate)}.`);
+      // }
+      
+
+    } catch (error) {
+      console.log(error);
+      console.log(`Error: ${error}`);
+      return res.status(500).json({
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+        status: 500,
+      });
+    }
+  }
 }
 
-export default PostsController;
+export default allPostsController;
--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -9,5 +9,19 @@ router.post("/post", (req, res) => {
   postsController.create(req, res);
 });
 
+router.get("/post", (req, res) => {
+  console.info(`> Request GET recieved in '/post' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  postsController.findAll(req, res);
+});
+
+router.get("/post/:id", (req, res) => {
+  console.info(`> Request GET recieved in '/post/:id' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  postsController.findById(req, res);
+});
+
+router.put("/post/:idOng/:idPost", (req, res) => {
+  console.info(`> Request PUT recieved in '/post/:idOng/:idPost' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  postsController.update(req, res);
+});
 
 export default router;
