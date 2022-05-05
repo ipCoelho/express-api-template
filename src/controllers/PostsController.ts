4964a78fc6134297f24ebf5813680f18d6534978
ipCoelho <ipcoelho.business@gmail.com>
2022-05-05 08:38:27 -0300

    feat: post creation feature finished

--- src/controllers/PostsController.ts
+++ src/controllers/PostsController.ts
@@ -4,7 +4,64 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 class PostsController {
-  
+  async create(req: Request, res: Response) {
+    try {
+      if (!req.body.idOng || !req.body.descricao) {
+        return res.status(400).json({
+          message: "Dados incompletos.",
+          expected: {
+            idOng: "number",
+            descricao: "string",
+
+          },
+          status: 400,
+        });
+      }
+
+      const postCreate = await prisma.tbl_post.create({
+        data: {
+          idOng: Number(req.body.idOng),
+          descricao: req.body.descricao,
+        }
+      });
+
+      console.log(`Post: `, postCreate);
+      
+
+      if (postCreate) {
+        const media = [];
+        if (req.body.media && req.body.media.length > 0) {
+          for (let i = 0; i < req.body.media.length; i++) {
+            const creatingMedia = await prisma.tbl_post_media.create({
+              data: {
+                titulo: req.body.media[i],
+                idOng: Number(req.body.idOng),
+                endereco: req.body.media[i],
+                idPost: postCreate.idPost,  
+              },
+            });
+            console.log(`Media[${i}]:\n ${JSON.stringify(creatingMedia)}.`);
+            media.push(creatingMedia);
+          }
+        }
+
+        return res.status(200).json({
+          message: `Post para ONG '${req.body.idOng}' cadastrado com sucesso.`,
+          textoEnviado: req.body.descricao,
+          status: 200,
+          data: { post: postCreate, media: media },
+        });
+      }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return res.status(500).json({
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+        status: 500,
+      });
+    }
+  }
+
+
 }
 
 export default PostsController;
--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -4,5 +4,10 @@ import { Router } from "express";
 const router = Router();
 const postsController = new PostsController();
 
+router.post("/post", (req, res) => {
+  console.info(`> Request POST recieved in '/post' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  postsController.create(req, res);
+});
+
 
 export default router;
