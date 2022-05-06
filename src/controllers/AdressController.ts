bb88bf815a44fd589d518c1cf19daa1d5f4b5630
ipCoelho <ipcoelho.business@gmail.com>
2022-05-06 14:43:20 -0300

    feat: adress features: findAll, findById, update and remove finisehd

--- src/controllers/AdressController.ts
+++ src/controllers/AdressController.ts
@@ -83,6 +83,181 @@ class AdressController {
       });
     }
   }
+
+  async findAll(req: Request, res: Response) {
+    try {
+      const adress = await prisma.tbl_endereco.findMany();
+
+      if (adress.length === 0) {
+        return res.status(200).json({
+          message: "Nenhum endereço cadastrado.",
+          status: 200,
+          data: adress,
+        });
+      }
+
+      return res.status(200).json({
+        message: "Todos endereços registrados retornados.",
+        status: 200,
+        data: adress,
+      });
+
+    } catch (error) {
+      console.log(`> Error: ${error}`);
+      return res.status(500).json({
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+        status: 500,
+      });
+    }
+  }
+
+  async findById(req: Request, res: Response) {
+    try {
+      const adress = await prisma.tbl_endereco.findUnique({
+        where: {
+          idOng: Number(req.params.id),
+        },
+      });
+
+      if (adress == null) {
+        return res.status(400).json({
+          message: `Endereço não encontrado para ONG '${req.params.id}'.`,
+          status: 400,
+          data: adress,
+        });
+      }
+
+      return res.status(200).json({
+        message: `Endereço encontrado para ONG '${req.params.id}'.`,
+        status: 200,
+        data: adress,
+      });
+
+    } catch (error) {
+      console.log(`> Error: ${error}`);
+      return res.status(500).json({
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+        status: 500,
+      });
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    try {
+      if (!req.body.cep &&
+        !req.body.bairro &&
+        !req.body.numero &&
+        !req.body.rua &&
+        !req.body.municipio &&
+        !req.body.estado &&
+        !req.body.complemento) {
+        return res.status(400).json({
+          message: "Dados incompletos.",
+          expected: {
+            idOng: "params-number",
+            cep: "string?",
+            bairro: "string?",
+            numero: "number?",
+            rua: "string?",
+            municipio: "string?",
+            estado: "string?",
+            complemento: "string?",
+          },
+          status: 400,
+        });
+      }
+
+      const adressVerify = await prisma.tbl_endereco.findUnique({
+        where: {
+          idOng: Number(req.params.id),
+        }
+      });
+
+      if (adressVerify == null) {
+        return res.status(400).json({
+          message: `Endereço não encontrado para ONG '${req.body.idOng}'.`,
+          status: 400,
+        });
+      }
+
+      const ufVerify = await prisma.tbl_estado.findUnique({
+        where: {
+          nome: req.body.estado,
+        },
+      });
+
+      if (ufVerify == null) {
+        return res.status(400).json({
+          message: `Estado '${req.body.estado}' não existe.`,
+          status: 400,
+        });
+      }
+
+      const { estado, ...body } = req.body;
+      const adressUpdate = await prisma.tbl_endereco.update({
+        where: {
+          idOng: Number(req.params.id),
+        },
+        data: {
+          ...body,
+          idEstado: ufVerify.idEstado,
+        }
+      });
+
+      if (adressUpdate != null) {
+        return res.status(200).json({
+          message: `Endereço atualizado com sucesso para ONG '${req.params.id}'.`,
+          status: 200,
+          data: adressUpdate,
+        });
+      }
+
+    } catch (error) {
+      console.log(`> Error: ${error}`);
+      return res.status(500).json({
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+        status: 500,
+      });
+    }
+  }
+
+  async remove(req: Request, res: Response) {
+    try {
+      const adressVerify = await prisma.tbl_endereco.findUnique({
+        where: {
+          idOng: Number(req.params.id),
+        }
+      });
+
+      if (adressVerify == null) {
+        return res.status(400).json({
+          message: `Endereço não encontrado para ONG '${req.params.id}'.`,
+          status: 400,
+        });
+      }
+
+      const adressDelete = await prisma.tbl_endereco.delete({
+        where: {
+          idOng: Number(req.params.id),
+        }
+      });
+
+      if (adressDelete != null) {
+        return res.status(200).json({
+          message: `Endereço deletado com sucesso para ONG '${req.params.id}'.`,
+          status: 200,
+          data: adressDelete,
+        });
+      }
+
+    } catch (error) {
+      console.log(`> Error: ${error}`);
+      return res.status(500).json({
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+        status: 500,
+      });
+    }
+  }
 }
 
 export default AdressController;
--- src/routes/adressRouter.ts
+++ src/routes/adressRouter.ts
@@ -9,4 +9,24 @@ router.post("/adress", (req, res) => {
   adressController.create(req, res)
 });
 
+router.get("/adress", (req, res) => {
+  console.info(`> Request GET recieved in '/adress' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  adressController.findAll(req, res)
+});
+
+router.get("/adress/:id", (req, res) => {
+  console.info(`> Request GET recieved in '/adress/:id' at ${new Date().toLocaleString()}. \n> req.params:`, req.params);
+  adressController.findById(req, res)
+});
+
+router.put("/adress/:id", (req, res) => {
+  console.info(`> Request PUT recieved in '/adress/:id' at ${new Date().toLocaleString()}. \n> req.body:`, req.body, `\n> req.params:`, req.params);
+  adressController.update(req, res)
+});
+
+router.delete("/adress/:id", (req, res) => {
+  console.info(`> Request DELETE recieved in '/adress/:id' at ${new Date().toLocaleString()}. \n> req.params:`, req.params);
+  adressController.remove(req, res)
+});
+
 export default router;
