3d4a8b3ed01186fdff184d7a20c4970b21dd5562
ipCoelho <ipcoelho.business@gmail.com>
2022-05-09 15:40:55 -0300

    feat: firebase file added

---
+++ src/auth&config/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+// import { firebaseConfig } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlxB-5oJDDKYwa8KiRrxi6KK_9iVyIQJk",
+  authDomain: "helpongs-c9e37.firebaseapp.com",
+  projectId: "helpongs-c9e37",
+  storageBucket: "helpongs-c9e37.appspot.com",
+  messagingSenderId: "13489519639",
+  appId: "1:13489519639:web:22a7e9c93b0d6a21a13052",
+  measurementId: "G-G1YDNTFYDZ"
+};
+
+const firebase = initializeApp(firebaseConfig);
+
