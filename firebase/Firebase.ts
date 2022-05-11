ff12964c6b20fa46f31915f2017708a2dd33469e
ipCoelho <ipcoelho.business@gmail.com>
2022-05-11 16:49:26 -0300

    feat: tests docs added

--- firebase/Firebase.ts
+++ firebase/Firebase.ts
@@ -1,18 +1,21 @@
 import admin from 'firebase-admin';
+import keyJson from './helpongs-firebase-key.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert("./helpongs-firebase-key.json"),
+  credential: admin.credential.cert(),
   databaseURL: "https://helpongs-c9e37-default-rtdb.firebaseio.com"
 });
 
-let data;
+const data = [];
 admin.firestore()
   .collection('cities')
   .get()
   .then((snapshot) => {
     snapshot.docs.forEach(doc => console.log(doc.data()));
-    data = snapshot.docs.map((doc) => {
-      return doc.data();
+    snapshot.docs.map((doc) => {
+      data.push({
+        ...doc.data()
+      });
     });
   });
 
