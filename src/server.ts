ced06a34fdd872e060db38689066b03e8e05b1af
ipCoelho <ipcoelho.business@gmail.com>
2022-05-12 08:53:58 -0300

    fix: reverted server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import express from 'express';
 import router from './router';
 import { API } from './API';
-import { data } from 'firebase/Firebase';
 
-const api = new API(express, "http://localhost:", process.env.API_PORT ?? 3030, router);
+ = new API(express, "http://localhost:", process.env.API_PORT ?? 3030, router);const api
 api.start();
-
-console.log(`data: `, data);
 
