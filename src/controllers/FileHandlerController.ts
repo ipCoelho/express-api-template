4e19258ff2f0231af7bd7fb72a2f239ffe988e73
ipCoelho <ipcoelho.business@gmail.com>
2022-05-12 10:28:30 -0300

    feat: mock feature for file uploading done

--- src/controllers/FileHandlerController.ts
+++ src/controllers/FileHandlerController.ts
@@ -1,19 +1,32 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 class FileHandlerController {
   async upload(req: Request, res: Response) {
     try {
-      const file = req.body.file;
-      console.log(`file: `, file);
-
-      if (file != null) {
-        return res.status(200).send(
-          file
-        )
+      let sampleFile = undefined, uploadPath = undefined;
+      if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({ 
+          message:'No files were sent.',
+          status: 400
+        });
       }
+
+      sampleFile = req.files.imagefile, uploadPath = __dirname + '/../temp/' + sampleFile.name;
+
+      sampleFile.mv(uploadPath, (error : unknown) => {
+        if (error) {
+          console.log(`> Error: ${error}`);
+          return res.status(500).json({
+            message: process.env.ERRO_500 ?? "Erro no servidor.",
+            status: 500,
+          });
+        } else {
+          return res.status(200).json({
+            message: `Arquivo '${sampleFile.name}' salvo com sucesso.`,
+            status: 200
+          });
+        }
+      });
     } catch (error) {
       console.log(`> Error: ${error}`);
       return res.status(500).json({
