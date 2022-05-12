b6892363027455302b1c17f5c4f6fc09e969b2ec
ipCoelho <ipcoelho.business@gmail.com>
2022-05-12 11:28:58 -0300

    wip-error: uploading files into firebase storage

--- src/controllers/FileHandlerController.ts
+++ src/controllers/FileHandlerController.ts
@@ -1,9 +1,27 @@
 import { Request, Response } from "express";
+import { FileArray } from "express-fileupload";
+import { initializeApp, getApp } from "firebase/app";
+import { getBlob, getStorage, ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlxB-5oJDDKYwa8KiRrxi6KK_9iVyIQJk",
+  authDomain: "helpongs-c9e37.firebaseapp.com",
+  databaseURL: "https://helpongs-c9e37-default-rtdb.firebaseio.com",
+  projectId: "helpongs-c9e37",
+  storageBucket: "helpongs-c9e37.appspot.com",
+  messagingSenderId: "13489519639",
+  appId: "1:13489519639:web:22a7e9c93b0d6a21a13052",
+  measurementId: "G-G1YDNTFYDZ"
+};
+
+initializeApp(firebaseConfig);
+const firebaseApp = getApp(), storage = getStorage(firebaseApp, "gs://helpongs-c9e37.appspot.com");
 
 class FileHandlerController {
   async upload(req: Request, res: Response) {
     try {
       let sampleFile = undefined, uploadPath = undefined;
+
       if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).json({ 
           message:'No files were sent.',
@@ -11,7 +29,19 @@ class FileHandlerController {
         });
       }
 
-      sampleFile = req.files.imagefile, uploadPath = __dirname + '/../temp/' + sampleFile.name;
+      sampleFile = req.files.imagefile;
+      uploadPath = __dirname + '/../temp/' + sampleFile.name;
+      console.log(`File: `, sampleFile);
+// Creating refs for the storage bucket and the path where the file will be uploaded.
+      const mediaRef = ref(storage, sampleFile.name);
+      const mediaFolderRef = ref(storage, `images/${sampleFile.name}`);
+// Parsing the file into a buffer, so the method uploadBytes() can understand it.
+      const abFile = new ArrayBuffer(sampleFile);
+      console.log(`ArrayBuffer: `, abFile);
+// By some reason, the file is uploaded, but it's size is empty.
+      uploadBytes(mediaFolderRef, abFile, { contentType: sampleFile.mimetype })
+        .then(obj => console.log(`Snapshot: `, obj)
+      );
 
       sampleFile.mv(uploadPath, (error : unknown) => {
         if (error) {
