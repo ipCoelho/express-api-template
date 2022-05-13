3ad27ff6e3362a1b06ee231c3ba369b18967e470
ipCoelho <ipcoelho.business@gmail.com>
2022-05-13 16:14:11 -0300

    feat: code refatored

--- src/controllers/FileHandlerController.ts
+++ src/controllers/FileHandlerController.ts
@@ -1,21 +1,5 @@
 import FirebaseHandler from "@utils/FirebaseHandler";
 import { Request, Response } from "express";
-import { initializeApp, getApp } from "firebase/app";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBlxB-5oJDDKYwa8KiRrxi6KK_9iVyIQJk",
-  authDomain: "helpongs-c9e37.firebaseapp.com",
-  databaseURL: "https://helpongs-c9e37-default-rtdb.firebaseio.com",
-  projectId: "helpongs-c9e37",
-  storageBucket: "helpongs-c9e37.appspot.com",
-  messagingSenderId: "13489519639",
-  appId: "1:13489519639:web:22a7e9c93b0d6a21a13052",
-  measurementId: "G-G1YDNTFYDZ"
-};
-
-initializeApp(firebaseConfig);
-const firebaseApp = getApp(), storage = getStorage(firebaseApp, "gs://helpongs-c9e37.appspot.com");
 
 class FileHandlerController {
   async upload(req: Request, res: Response) {
@@ -24,11 +8,16 @@ class FileHandlerController {
       const firebase = new FirebaseHandler();
       const status = await firebase.uploadImage(file, file.mimetype);
 
-      if (status === true) {
+       if (status === true) {
         res.status(200).json({
           message: `Arquivo '${file.originalname}' salvo com sucesso.`,
           resolve: status,
           status: 200,  
+        });
+      } else if (status === false) {
+        return res.status(500).json({
+          message: process.env.ERRO_500 ?? "Erro interno do servidor.",
+          status: 500
         });
       }
     } catch (error) {
