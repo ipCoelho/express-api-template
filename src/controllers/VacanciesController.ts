d561309da7fa6a92f63e827a174982ea10a603a8
ipCoelho <ipcoelho.business@gmail.com>
2022-05-16 16:36:50 -0300

    wip: working in vacancies feature

--- src/controllers/VacanciesController.ts
+++ src/controllers/VacanciesController.ts
@@ -1,35 +1,78 @@
 import { Request, Response } from "express";
-import { PrismaClient, tbl_contato, tbl_endereco, tbl_vagas } from "@prisma/client";
+import { PrismaClient, tbl_endereco } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 class VacanciesController {
-  createVacancy(req: Request, res: Response) {
-    interface Vacancy {
-      titulo: string;
-      descricao: string;
-      requisitos: string;
-      cargaHoraria: string;
-    }
+  async createVacancy(req: Request, res: Response) {
     try {
       const idOng: number = req.body.idOng;
-      const vaga: tbl_vagas = req.body.vaga;
-      const contato: tbl_contato = req.body.contato;
-      const endereco: tbl_endereco = req.body.endereco;
+      const vaga: Vacancy = req.body.vaga;
+      const endereco: Adress = req.body.endereco;
 
-      // const 
+      const vacancyRecord = await prisma.tbl_vagas.create({
+        data: {
+          idOng: Number(idOng),
+          cargaHoraria: vaga.cargaHoraria,
+          descricao: vaga.descricao,
+          requisitos: vaga.requisitos,
+          titulo: vaga.titulo,
+          // idEndereco
 
-      
-      
+        }
+      });
+      console.log("Registro de Vaga: ", vacancyRecord);
+
+      const addressRecord = await prisma.tbl_endereco.create({
+        data: { idEstado: Number(endereco.idEstado), ...endereco }
+      });
+      console.log("Registro de Endereço: ", addressRecord);
+
 
     } catch (error) {
       console.log(`> Error: ${error}`);
       return res.status(500).json({
         message: process.env.ERRO_500 ?? "Erro no servidor.",
         status: 500,
+        expected: {
+          idOng: "number",
+          vaga: {
+            titulo: "string",
+            descricao: "string",
+            requisitos: "string",
+            cargaHoraria: "string",
+          },
+          endereco: {
+            idLogin: "number",
+            idEstado: "number",
+            bairro: "string",
+            numero: "number",
+            cep: "string",
+            rua: "string",
+            complemento: "string",
+            municipio: "string",
+          }
+        }
       });
     }
   }
+}
+
+interface Vacancy {
+  titulo: string;
+  descricao: string;
+  requisitos: string;
+  cargaHoraria: string;
+}
+
+interface Adress {
+  idEstado: number;
+  bairro: string;
+  numero: number;
+  rua: string;
+  cep: string;
+  complemento: string;
+  municipio: string;
 }
 
 export default VacanciesController;
--- src/routes/vacanciesRouter.ts
+++ src/routes/vacanciesRouter.ts
@@ -4,6 +4,9 @@ import { Router } from "express";
 const router = Router();
 const vacanciesController = new VacanciesController();
 
-
+router.post("/vacancy", (req, res) => {
+  console.info(`> Request POST recieved in '/vacancy' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  vacanciesController.createVacancy(req, res);
+});
 
 export default router;
