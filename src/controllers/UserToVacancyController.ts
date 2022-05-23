30c4e4c40b2ad2d9c4ec35eed5098b2e79847579
ipCoelho <ipcoelho.business@gmail.com>
2022-05-23 15:07:58 -0300

    feat: UserToVacancyController.all() finished (crud ops)

--- src/controllers/UserToVacancyController.ts
+++ src/controllers/UserToVacancyController.ts
@@ -4,7 +4,193 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 class UserToVacancyController {
+  async createUserToVacancy(req: Request, res: Response) {
+    try {
+      if (!req.body.idVagas || !req.body.idUsuario) {
+        return res.status(400).json({
+          status: 400,
+          message: "Dados incompletos.",
+          expected: {
+            idVagas: "number",
+            idUsuario: "number",
+          },
+        });
+      }
 
+      const idVagas: number = req.body.idVagas;
+      const idUsuario: number = req.body.idUsuario;
+
+      const vacancyMask = await prisma.tbl_vagas.findUnique({
+        where: { idVagas: Number(idVagas) }
+      });
+
+      if (vacancyMask == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Vaga com ID '${idVagas}' não encontrada.`,
+        });
+      }
+
+      const userMask = await prisma.tbl_usuario.findUnique({
+        where: { idUsuario: Number(idUsuario) }
+      });
+
+      if (userMask == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Usuário com ID '${idUsuario}' não encontrado.`,
+        });
+      }
+
+      const userToVacancyMask = await prisma.tbl_vagas_usuario.findFirst({
+        where: { idUsuario: Number(idUsuario), idVagas: Number(idVagas) }
+      });
+
+      if (userToVacancyMask != null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Usuário com ID '${idUsuario}' já está inscrito na vaga com ID '${idVagas}'.`,
+        });
+      }
+
+      const userToVacancy = await prisma.tbl_vagas_usuario.create({
+        data: {
+          idVagas: Number(idVagas),
+          idUsuario: Number(idUsuario),
+        },
+        include: {
+          tbl_usuario: true,
+          tbl_vagas: true
+        }
+      });
+
+      return res.status(200).json({
+        message: `Usuário com ID '${idUsuario}' inscrito na vaga com ID '${idVagas}' com sucesso.`,
+        status: 200,
+        data: userToVacancy,
+      });
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return res.status(500).json({
+        status: 500,
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+      });
+    } 
+  }
+
+  async allVacanciesPerOng(req: Request, res: Response) {
+    try {
+      const idOng = Number(req.params.idOng);
+
+      const vacancies = await prisma.tbl_vagas.findMany({
+        where: { idOng: Number(idOng) },
+        include: {
+          tbl_contato: true,
+          tbl_endereco: true,
+          tbl_ong: true,
+          tbl_vagas_usuario: {
+            select: {
+              idUsuario: true,
+              idVagas: true,
+              tbl_usuario: true,
+              tbl_vagas: true
+            }
+          }
+        }
+      });
+
+      return res.status(200).json({
+        status: 200,
+        message: "Lista de vagas encontrada com sucesso.",
+        data: vacancies,
+      });
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return res.status(500).json({
+        status: 500,
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+      });
+    }
+  }
+
+  async findUniqueVacancy(req: Request, res: Response) {
+    try {
+      const idOng = Number(req.params.idOng);
+      const idVaga = Number(req.params.idVaga);
+
+      const allOngsVacancies = await prisma.tbl_vagas.findMany({
+        where: { idOng: Number(idOng) },
+        include: {
+          tbl_contato: true,
+          tbl_endereco: true,
+          tbl_ong: true,
+          tbl_vagas_usuario: {
+            select: {
+              idUsuario: true,
+              idVagas: true,
+              tbl_usuario: true,
+              tbl_vagas: true
+            }
+          }
+        }
+      });
+
+      const requestedVacancy = allOngsVacancies.find((vacancy) => vacancy.idVagas == idVaga);
+
+      if (requestedVacancy == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Vaga com ID '${idVaga}' não encontrada.`,
+        });
+      } else {
+        return res.status(200).json({
+          status: 200,
+          message: "Vaga encontrada com sucesso.",
+          data: requestedVacancy,
+        });
+      }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return res.status(500).json({
+        status: 500,
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+      });
+    }
+  }
+
+  async removeUserToVacancy(req: Request, res: Response) {
+    try {
+      const idVagas = Number(req.params.idVagas);
+      const idUsuario = Number(req.params.idUsuario);
+
+      const findRecord = await prisma.tbl_vagas_usuario.findFirst({
+        where: { idUsuario: Number(idUsuario), idVagas: Number(idVagas) }
+      });
+
+      if (findRecord == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Usuário com ID '${idUsuario}' não está inscrito na vaga com ID '${idVagas}'.`,
+        });
+
+      } else {
+        const recordDelete = await prisma.tbl_vagas_usuario.delete({
+          where: { idVagasUsuario: Number(findRecord.idVagasUsuario) }
+        });
+        return res.status(200).json({
+          status: 200,
+          message: `Usuário com ID '${idUsuario}' desinscrito da vaga com ID '${idVagas}' com sucesso.`,
+          data: recordDelete,
+        });
+      }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return res.status(500).json({
+        status: 500,
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+      });
+    }
+  }
 }
 
 export default UserToVacancyController;
--- src/routes/userToVacancyRouter.ts
+++ src/routes/userToVacancyRouter.ts
@@ -4,4 +4,24 @@ import { Router } from "express";
 const router = Router();
 const userToVacancyController = new UserToVacancyController();
 
+router.post("/vacancy-controller", (req, res) => {
+  console.info(`> Request POST recieved in '/vacancy-controller' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  userToVacancyController.createUserToVacancy(req, res);
+});
+
+router.get("/vacancy-controller/:idOng", (req, res) => {
+  console.info(`> Request GET recieved in '/vacancy-controller' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  userToVacancyController.allVacanciesPerOng(req, res);
+});
+
+router.get("/vacancy-controller/:idOng/:idVagas", (req, res) => {
+  console.info(`> Request GET recieved in '/vacancy-controller' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  userToVacancyController.findUniqueVacancy(req, res);
+});
+
+router.delete("/vacancy-controller/:idVagas/:idUsuario", (req, res) => {
+  console.info(`> Request DELETE recieved in '/vacancy-controller' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  userToVacancyController.removeUserToVacancy(req, res);
+});
+
 export default router;
