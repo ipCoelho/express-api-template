f05390cb6ebf94c1e4081fc2d0a02e22687b347a
ipCoelho <ipcoelho.business@gmail.com>
2022-05-23 13:35:44 -0300

    feat: UserToEvent.create() finished

--- src/controllers/UserToEventController.ts
+++ src/controllers/UserToEventController.ts
@@ -1,7 +1,78 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
+
 class UserToEventController {
+  async createUserToEvent(req: Request, res: Response) {
+    try {
+      if (!req.body.idEvento || !req.body.idUsuario) {
+        return res.status(400).json({
+          status: 400,
+          message: "Dados incompletos.",
+          expected: {
+            idEvento: "number",
+            idUsuario: "number",
+          },
+        });
+      }
+
+      const idEvento: number = req.body.idEvento;
+      const idUsuario: number = req.body.idUsuario;
+
+      const eventMask = await prisma.tbl_eventos.findUnique({
+        where: { idEventos: Number(idEvento) }
+      });
+
+      if (eventMask == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Evento com ID '${idEvento}' não encontrado.`,
+        });
+      }
+
+      const userMask = await prisma.tbl_usuario.findUnique({
+        where: { idUsuario: Number(idUsuario) }
+      });
+
+      if (userMask == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Usuário com ID '${idUsuario}' não encontrado.`,
+        });
+      }
+
+      const userToEventMask = await prisma.tbl_usuario_evento.findFirst({
+        where: { idUsuario: Number(idUsuario), idEventos: Number(idEvento) }
+      });
+
+      if (userToEventMask != null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Usuário com ID '${idUsuario}' já está inscrito no evento com ID '${idEvento}'.`,
+        });
+      }
+
+      const userToEvent = await prisma.tbl_usuario_evento.create({
+        data: {
+          idEventos: Number(idEvento),
+          idUsuario: Number(idUsuario),
+        },
+      });
+
+      return res.status(200).json({
+        status: 200,
+        message: `Usuário com ID '${idUsuario}' inscrito no evento com ID '${idEvento}' com sucesso.`,
+        data: userToEvent,
+      });
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      return res.status(500).json({
+        status: 500,
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+      });
+    }
+  }
 
 }
 
--- src/routes/userToEventRouter.ts
+++ src/routes/userToEventRouter.ts
@@ -2,5 +2,11 @@ import UserToEventController from "@controllers/UserToEventController";
 import { Router } from "express";
 
 const router = Router();
+const userToEventController = new UserToEventController();
+
+router.post("/event-controller", (req, res) => {
+  console.info(`> Request POST recieved in '/event-controller' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  userToEventController.createUserToEvent(req, res);
+});
 
 export default router;
