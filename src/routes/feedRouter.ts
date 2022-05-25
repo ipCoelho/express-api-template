bc032ee554a1bb841bed00ce9dce5d89a710440a
ipCoelho <ipcoelho.business@gmail.com>
2022-05-25 15:39:40 -0300

    feat: feeds for vacancies, posts and events

--- src/routes/feedRouter.ts
+++ src/routes/feedRouter.ts
@@ -9,4 +9,9 @@ router.get("/feed/:id", (req, res) => {
   feedController.buildFeed(req, res);
 });
 
+router.get("/feed/:type/:page", (req, res) => {
+  console.info(`> Request GET recieved in '/feed/:type/:page' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  feedController.buildSpecificFeed(req, res);
+});
+
 export default router;
