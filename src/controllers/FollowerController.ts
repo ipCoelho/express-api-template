d1fdf74c322d949034eb5f085a7b1f7ef1de4d87
ipCoelho <ipcoelho.business@gmail.com>
2022-05-26 13:25:26 -0300

    feat: follower by ong and by user finished

--- src/controllers/FollowerController.ts
+++ src/controllers/FollowerController.ts
@@ -77,6 +77,148 @@ class FollowerController {
       });
     }
   }
+
+  async stopFollowing(req: Request, res: Response) {
+    try {
+      if (!req.params.idUsuario || !req.params.idOng) {
+        return res.status(400).json({
+          status: 400,
+          message: "Dados incompletos.",
+          expected: {
+            idUsuario: "number",
+            idOng: "number",
+          },
+        });
+      }
+
+      const idUsuario = Number(req.params.idUsuario);
+      const idOng = Number(req.params.idOng);
+
+      const userMask = await prisma.tbl_usuario.findUnique({
+        where: { idUsuario: Number(idUsuario) }
+      });
+
+      if (userMask == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Usuário com ID '${idUsuario}' não encontrado.`,
+        });
+      }
+
+      const ongMask = await prisma.tbl_ong.findUnique({
+        where: { idOng: Number(idOng) }
+      });
+
+      if (ongMask == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `ONG com ID '${idOng}' não encontrada.`,
+        });
+      }
+
+      const followerMask = await prisma.tbl_seguidor.findFirst({
+        where: { idUsuario: Number(idUsuario), idOng: Number(idOng) }
+      });
+
+      if (followerMask == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Usuário com ID '${idUsuario}' não está seguindo a ONG com ID '${idOng}'.`,
+        });
+      }
+
+      await prisma.tbl_seguidor.delete({
+        where: { idSeguidor: followerMask.idSeguidor }
+      });
+
+      return res.status(200).json({
+        message: `Usuário com ID '${idUsuario}' não está mais seguindo a ONG com ID '${idOng}'.`,
+        status: 200,
+        data: followerMask
+      });
+    } catch (error) {
+      console.log(`Error: `, error);
+      return res.status(500).json({
+        status: 500,
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+      });
+    }
+  }
+
+  async getFollowersForOng(req: Request, res: Response) {
+    try {
+      const idOng = Number(req.params.idOng);
+
+      const ongMask = await prisma.tbl_ong.findUnique({
+        where: { idOng: Number(idOng) }
+      });
+
+      if (ongMask == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `ONG com ID '${idOng}' não encontrada.`,
+        });
+      }
+
+      const followers = await prisma.tbl_seguidor.findMany({
+        where: { idOng: Number(idOng) },
+        include: {
+          tbl_ong: true,
+          tbl_usuario: true
+        }
+      });
+
+      return res.status(200).json({
+        message: `Lista de usuários seguindo ONG com ID '${idOng}' encontrada.`,
+        status: 200,
+        data: followers
+      });
+    } catch (error) {
+      console.log(`Error: `, error);
+      return res.status(500).json({
+        status: 500,
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+      });
+    }
+  }
+
+  async getOngsForUser(req: Request, res: Response) {
+    try {
+      const idUsuario = Number(req.params.idUsuario);
+
+      const userMask = await prisma.tbl_usuario.findUnique({
+        where: { idUsuario: Number(idUsuario) }
+      });
+
+      if (userMask == null) {
+        return res.status(400).json({
+          status: 400,
+          message: `Usuário com ID '${idUsuario}' não encontrado.`,
+        });
+      }
+
+      const ongs = await prisma.tbl_seguidor.findMany({
+        where: { idUsuario: Number(idUsuario) },
+        include: {
+          tbl_ong: true,
+          tbl_usuario: true
+        }
+      });
+
+      return res.status(200).json({
+        message: `Lista de ONGs seguidas pelo usuário com ID '${idUsuario}'`,
+        status: 200,
+        data: ongs
+      });
+    } catch (error) {
+      console.log(`Error: `, error);
+      return res.status(500).json({
+        status: 500,
+        message: process.env.ERRO_500 ?? "Erro no servidor.",
+      });
+    }
+
+  }
 }
 
 export default FollowerController;
--- src/routes/followerRouter.ts
+++ src/routes/followerRouter.ts
@@ -9,4 +9,20 @@ router.post("/follower", (req, res) => {
   followerController.startFollowing(req, res);
 });
 
+router.delete("/follower/:idOng/:idUsuario", (req, res) => {
+  console.info(`> Request DELETE recieved in '/follower' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  followerController.stopFollowing(req, res);
+});
+
+router.get("/follower/ong/:idOng", (req, res) => {
+  console.info(`> Request GET recieved in '/follower' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  followerController.getFollowersForOng(req, res);
+});
+
+router.get("/follower/user/:idUsuario", (req, res) => {
+  console.info(`> Request GET recieved in '/follower' at ${new Date().toLocaleString()}. \n> req.body:`, req.body);
+  followerController.getOngsForUser(req, res);
+});
+
+
 export default router;
